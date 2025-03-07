625ae811454ed8fe18cb764416abaf6570d91731
lynnliu030 <lshu@berkeley.edu>
2025-03-07 23:19:02 +0000

    fix

--- download_dataset.sh
+++ download_dataset.sh
@@ -3,5 +3,5 @@ curl -O https://s3.amazonaws.com/mlsys-artifact/squad.csv
 curl -O https://s3.amazonaws.com/mlsys-artifact/fever_reordered.csv
 curl -O https://s3.amazonaws.com/mlsys-artifact/fever_with_evidence_5.csv
 
-# mv fever.csv squad.csv datasets/
+mv fever.csv squad.csv ./datasets/
 mv fever_with_evidence_5.csv fever_reordered.csv ./run/accuracy/datasets/
